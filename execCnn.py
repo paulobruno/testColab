17615c9138ed4fbaa423d6b51bf214da5c382958
Paulo Bruno <pbrunosousa@gmail.com>
2018-08-09 09:27:37 -0300

    Execute CNN in directory

--- execCnn.py
+++ execCnn.py
@@ -8,41 +8,43 @@ import numpy as np
 import tensorflow as tf
 import sys
 import time
-
-
-start_time = time.time()
+import glob
   
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
+def main(unused_argv):
+
+  if len(sys.argv) > 1:
+    path = sys.argv[1]
+  else:
     print ('Erro: nenhum arquivo passado')
     sys.exit()
-
-
-def main(unused_argv):
-  
+      
   # Create the Estimator
   roi_classifier = tf.estimator.Estimator(
       model_fn=cnn_model_fn, model_dir="models/roi_convnet_model_new")
-
-  new_samples = np.loadtxt(filename, dtype=int, delimiter=' ')  
-  new_samples = new_samples.astype(float) / 255.0
-  
-  predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+    
+  files = glob.glob(path + '*.txt')
+    
+  for filename in files:
+    print("filename: " + filename)
+    new_samples = np.loadtxt(filename, dtype=int, delimiter=' ')
+    new_samples = new_samples.astype(float) / 255.0
+      
+    predict_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": new_samples},
       num_epochs=1,
       shuffle=False)
-
-  predictions = list(roi_classifier.predict(input_fn=predict_input_fn))
-  predicted_classes = [p["classes"] for p in predictions]
-    
-  np.savetxt(filename + "_out.txt", np.transpose(predicted_classes), fmt='%d', newline=' ')
+      
+    start_time = time.time()
+    predictions = list(roi_classifier.predict(input_fn=predict_input_fn))
+    print("--- %s seconds ---" % (time.time() - start_time))
   
-  print("--- %s seconds ---" % (time.time() - start_time))
-
+    predicted_classes = [p["classes"] for p in predictions]
+    
+    np.savetxt(filename + "_out.txt", np.transpose(predicted_classes), fmt='%d', newline=' ')
+  
 
 if __name__ == "__main__":
   tf.app.run()
