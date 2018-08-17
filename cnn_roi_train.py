c384e2ebd47db96d11817ede32b3ff405fc5e304
Paulo Bruno <pbrunosousa@gmail.com>
2018-08-17 08:17:13 -0300

    Decrease max steps number

--- cnn_roi_train.py
+++ cnn_roi_train.py
@@ -43,7 +43,7 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('train_dir', 'cnn_roi_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 200000,
+tf.app.flags.DEFINE_integer('max_steps', 100000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
